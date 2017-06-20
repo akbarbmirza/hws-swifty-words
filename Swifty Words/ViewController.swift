8220ae18c70fec433ff0bc276036a518a97eab97
Akbar Mirza <akbarbmirza@gmail.com>
2017-06-20 00:19:42 -0400

    Add Elements to Storyboard for App

--- Swifty Words/ViewController.swift
+++ Swifty Words/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // =========================================================================
+    // Outlets
+    // =========================================================================
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    // =========================================================================
+    // Properties
+    // =========================================================================
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +32,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // =========================================================================
+    // Actions
+    // =========================================================================
+    
+    @IBAction func submitTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    
 }
 
